d413fa8b6832a51b03e75ffa1decde06873b19d9
binyam <binyamawiw@gmail.com>
2025-08-15 22:03:47 +0300

    new

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -1,5 +1,5 @@
 // src/pages/StorePage.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import supabaseClient from '../lib/supabaseClient';
 
